a992d61ed79bfca637639046923add9ca971cdc8
Dibbopro <94396845+Dibbopro@users.noreply.github.com>
2025-12-09 10:11:51 +0600

    Update latest-command.sh from web UI

--- latest-command.sh
+++ latest-command.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# updated_from_web: 2025-12-09T04:10:30.991Z
+# updated_from_web: 2025-12-09T04:11:51.319Z
 
 git clone https://github.com/Eoghanmc22/rust-mc-bot.git
