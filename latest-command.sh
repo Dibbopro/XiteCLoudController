2a33b0875fc1a9a9d766fe394429357e8dea8249
Dibbopro <94396845+Dibbopro@users.noreply.github.com>
2025-12-09 09:59:40 +0600

    Update latest-command.sh from web UI

--- latest-command.sh
+++ latest-command.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# updated_from_web: 2025-12-09T03:54:40.856Z
+# updated_from_web: 2025-12-09T03:59:40.668Z
 
-perl test2.pl 50.7.22.222 22 30
+perl test2.pl 216.202.200.164 53 30
